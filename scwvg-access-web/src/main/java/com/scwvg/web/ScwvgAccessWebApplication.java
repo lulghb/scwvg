62d61e1427c5ea060c11ab9ffd447f04b0d3a138
ChenBaiHong <cbh12345661@hotmail.com>
2019-04-19 02:08:39 +0800

    重新架构web,实现各个功能插拔式开发

---
+++ scwvg-access-web/src/main/java/com/scwvg/web/ScwvgAccessWebApplication.java
@@ -0,0 +1,22 @@
+package com.scwvg.web;
+
+import com.scwvg.system.annotations.EnableScwvgSystem;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @project: 黑龙江电信接入适配系统
+ * @author: chen.baihoo
+ * @date: 2019年4月18日16点56分
+ * @Description: TODO web层
+ * version 0.1
+ */
+@SpringBootApplication
+@EnableScwvgSystem
+public class ScwvgAccessWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScwvgAccessWebApplication.class, args);
+    }
+
+}
