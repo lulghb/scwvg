4be9045869d283a68000976a0af02672fd9be7d2
陆郎 <18482297774@189.cn>
2019-04-26 10:18:01 +0800

    重构

--- scwvg-pmee-consumer/src/main/java/com/scwvg/controller/FluxConsumer.java
+++ scwvg-pmee-consumer/src/main/java/com/scwvg/controller/FluxConsumer.java
@@ -3,14 +3,10 @@ package com.scwvg.controller;
 import com.scwvg.entitys.FluxEntity;
 import com.scwvg.service.FluxClientService;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * @aothor: lul
@@ -26,22 +22,6 @@ public class FluxConsumer {
     @RequestMapping(value = "/consumer/list")
     public List<FluxEntity> list() {
         return fcs.getaFlux();
-    }
-
-    @RequestMapping(value = "/userLogin")
-    @ResponseBody
-    public Map userLogin(HttpServletRequest request) {
-        Map res = new HashMap();
-        String userName = request.getParameter("username");
-        String password = request.getParameter("password");
-        String vercode = request.getParameter("vercode");
-        String remember = request.getParameter("remember");
-        System.out.println("获取到的用户名是：" + userName + "密码是：" + password + "验证码是：" + vercode);
-
-        res.put("code", "0");
-        res.put("msg", "登录成功");
-        res.put("data", "access_token:c262e61cd13");
-        return res;
     }
     /**
      * 服务发现
