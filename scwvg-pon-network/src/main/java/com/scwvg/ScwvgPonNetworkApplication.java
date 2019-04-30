da9acce816cbcecbdd7a42d3d611aec18044c792
陆郎 <18482297774@189.cn>
2019-04-30 09:57:05 +0800

    规范化PON网管项目，架构使用mybatis

---
+++ scwvg-pon-network/src/main/java/com/scwvg/ScwvgPonNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.scwvg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScwvgPonNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScwvgPonNetworkApplication.class, args);
+    }
+
+}
