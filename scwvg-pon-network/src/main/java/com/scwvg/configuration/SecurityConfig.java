cb417bab71defa07bc6130c1f948faeee93bfc80
陆郎 <18482297774@189.cn>
2019-05-03 22:08:06 +0800

    SecurityConfig

---
+++ scwvg-pon-network/src/main/java/com/scwvg/configuration/SecurityConfig.java
@@ -0,0 +1,106 @@
+package com.scwvg.configuration;
+
+import com.scwvg.filter.TokenFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+
+/**
+ * @aothor: lul
+ * @unit: 智谷园网络科技有限公司
+ * @iphone:18482297774
+ * @date：20192019/5/1
+ * @Explain：Security 核心配置
+ **/
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true) //方法级别权限
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    /**
+     * 登录成功处理
+     */
+    @Autowired
+    private AuthenticationSuccessHandler authenticationSuccessHandler;
+    @Autowired
+    /**
+     * 登录失败处理
+     */
+    private AuthenticationFailureHandler authenticationFailureHandler;
+    @Autowired
+    /**
+     * 退出登录
+     */
+    private LogoutSuccessHandler logoutSuccessHandler;
+    @Autowired
+    /**
+     * 其他异常
+     */
+    private AuthenticationEntryPoint authenticationEntryPoint;
+    @Autowired
+    private UserDetailsService userDetailsService;
+    @Autowired
+    private TokenFilter tokenFilter;
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    /*重写configure方法*/
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        String [] staticres = new String[]{
+                "/",
+                "/capctha/**",
+                "/index",
+                "/users/current",
+                "/home/**",
+                "/js/**",
+                "/login",
+                "/wvglogin/**",
+                "/json/**",
+                "/layui/**",
+                "/lib/**",
+                "/modules/**",
+                "/style/**",
+                "/tpl/**",
+                "/templates/**",
+                "/user/**",
+                "/config.js/**",
+        };
+        http.csrf().disable();
+        // 基于token，所以不需要session
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests().antMatchers(staticres)
+                .permitAll().anyRequest().authenticated();
+        http.formLogin().loginProcessingUrl("/login")
+                .successHandler(authenticationSuccessHandler)
+                .failureHandler(authenticationFailureHandler).and()
+                .exceptionHandling().authenticationEntryPoint(authenticationEntryPoint);
+        http.logout().logoutUrl("/logout").logoutSuccessHandler(logoutSuccessHandler);
+        // 解决不允许显示在iframe的问题
+        http.headers().frameOptions().disable();
+        http.headers().cacheControl();
+
+        http.addFilterBefore(tokenFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+    }
+}
