cbeadaa5031afa0e3a01e9be674d4230fe7b6721
陆郎 <18482297774@189.cn>
2019-05-21 17:07:13 +0800

    专业管理

--- scwvg-api/src/main/java/com/scwvg/entitys/scwvgponnetwork/WvgSpecType.java
+++ scwvg-api/src/main/java/com/scwvg/entitys/scwvgponnetwork/WvgSpecType.java
@@ -1,5 +1,9 @@
 package com.scwvg.entitys.scwvgponnetwork;
 
+import jdk.nashorn.internal.objects.annotations.Setter;
+import lombok.Data;
+import lombok.Getter;
+
 import java.io.Serializable;
 
 /**
@@ -9,8 +13,10 @@ import java.io.Serializable;
  * @date：20192019/5/7
  * @Explain：专业管理
  **/
-public class WvgSpecType implements Serializable {
+@Data
+public class WvgSpecType extends BaseEntity<Long> implements Serializable {
     private static final long serialVersionUID = -6525908145032868837L;
     private Long  spec_id;     //专业ID
     private String spec_name;  //专业名称
+    private Long wvg_user_id;  //专业新增人
 }
